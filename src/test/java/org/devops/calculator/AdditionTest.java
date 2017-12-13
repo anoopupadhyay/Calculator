b78e1883cdbd19297ad3cae391786f5ee0643083
rajameka <rrajsekhar2@gmail.com>
2017-12-13 21:31:54 +0530

    updated the sys out messages

--- src/test/java/org/devops/calculator/AdditionTest.java
+++ src/test/java/org/devops/calculator/AdditionTest.java
@@ -23,14 +23,14 @@ public class AdditionTest extends TestCase{
 	}
 	public void testSumOfTwoOneBigPositiveAndNegativeNumbers() {
 		assertEquals(100.00,add.addition(400,-300));
-		System.out.println("testSumOfTwoOnePositiveAndNegativeNumbers: "+add.addition(400,-300));
+		System.out.println("testSumOfTwoOneBigPositiveAndNegativeNumbers: "+add.addition(400,-300));
 	}
 	public void testSumOfTwoOneFloatAndIntegerNumbers() {
 		assertEquals(700.50,add.addition(400.50,300));
-		System.out.println("testSumOfTwoOnePositiveAndNegativeNumbers: "+add.addition(400.50,300));
+		System.out.println("testSumOfTwoOneFloatAndIntegerNumbers: "+add.addition(400.50,300));
 	}
 	public void testSumOfTwoFloatNumbers() {
 		assertEquals(700.00,add.addition(400.50,299.50));
-		System.out.println("testSumOfTwoOnePositiveAndNegativeNumbers: "+add.addition(400.50,299.50));
+		System.out.println("testSumOfTwoFloatNumbers: "+add.addition(400.50,299.50));
 	}
 }
