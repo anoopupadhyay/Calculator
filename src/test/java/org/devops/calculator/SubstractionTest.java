65c9b70ecea653ac2a8311c04a808bd617e9d8c7
rajameka <rrajsekhar2@gmail.com>
2017-12-13 21:51:57 +0530

    Update SubstractionTest.java

--- src/test/java/org/devops/calculator/SubstractionTest.java
+++ src/test/java/org/devops/calculator/SubstractionTest.java
@@ -10,7 +10,7 @@ public class SubstractionTest extends TestCase{
 	}
 
 	public void testSubOfTwoPositiveNumbers() {
-		assertEquals(-100.00,subs.sub(300,400));
+		assertEquals(-300.00,subs.sub(300,400));
 		System.out.println("testSumOfTwoPositiveNumbers: "+subs.sub(300,400));
 	}
 	public void testSubOfTwoNegativeNumbers() {
@@ -18,7 +18,7 @@ public class SubstractionTest extends TestCase{
 		System.out.println("testSumOfTwoNegativeNumbers: "+subs.sub(-400,-300));
 	}
 	public void testSubOfOneNegativeAndOnePositive() {
-		assertEquals(700.00,subs.sub(400,-300));
+		assertEquals(800.00,subs.sub(400,-300));
 		System.out.println("testSumOfTwoNegativeNumbers: "+subs.sub(400,-300));
 	}	
 }
