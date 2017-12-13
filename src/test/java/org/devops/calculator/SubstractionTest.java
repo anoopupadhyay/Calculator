c822a87f84872ee11b22c7f40f3de493677f8e6e
Raja <rrajsekhar2@gmail.com>
2017-12-13 21:46:41 +0530

    adding subtraction test cases

---
+++ src/test/java/org/devops/calculator/SubstractionTest.java
@@ -0,0 +1,24 @@
+package org.devops.calculator;
+
+import junit.framework.TestCase;
+
+public class SubstractionTest extends TestCase{
+	Substraction subs=new Substraction();
+
+	public SubstractionTest(String name) {
+		super(name);
+	}
+
+	public void testSubOfTwoPositiveNumbers() {
+		assertEquals(-100.00,subs.sub(300,400));
+		System.out.println("testSumOfTwoPositiveNumbers: "+subs.sub(300,400));
+	}
+	public void testSubOfTwoNegativeNumbers() {
+		assertEquals(-100.00,subs.sub(-400,-300));
+		System.out.println("testSumOfTwoNegativeNumbers: "+subs.sub(-400,-300));
+	}
+	public void testSubOfOneNegativeAndOnePositive() {
+		assertEquals(700.00,subs.sub(400,-300));
+		System.out.println("testSumOfTwoNegativeNumbers: "+subs.sub(400,-300));
+	}	
+}
